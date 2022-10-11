0bd74899b094808108ac2e2b67821bef9f2a20a8
zeilmann <81104190+zeilmann@users.noreply.github.com>
2022-10-11 07:41:02 +0100

    third commit

--- src/filters/StringFilter.js
+++ src/filters/StringFilter.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 
-/**
- * Storage of several reusable functions related to string values
- */
+
 Vue.filter('formatStringDate', (value) => {
   if (value) {
     return new Date(value).toDateString();
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,13 +5,13 @@
 </template>
 
 <script>
-import RandomHeroes from '../components/heroes/RandomHeroes.vue';
+
 import ListAllHeroes from '../components/heroes/ListAllHeroes.vue';
 
 export default {
   name: 'Home',
   components: {
-    RandomHeroes,
+    
     ListAllHeroes
 },
 };
